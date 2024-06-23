16f6d9646a9d54f139d2c8892e4cea7e18e908ba
Daniel A. Caballero <danielalejandrocc33@gmail.com>
2024-06-23 23:28:46 +0200

    test vite config 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,10 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.NODE_ENV === 'production'
-      ? process.env.API_BASE_URL
-      : 'http://localhost:3000/',
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dir, './src')
@@ -17,7 +13,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target:
+          process.env.NODE_ENV === 'production'
+            ? process.env.API_BASE_URL
+            : 'http://localhost:3000',
         changeOrigin: true
       }
     }
